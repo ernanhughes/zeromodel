bca487d9aa59dd5a6500c7b7907d41da9257e370
ernanhughes <155574808+ernanhughes@users.noreply.github.com>
2025-08-04 20:01:56 +0100

    Fix test expectations for tile size

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ZeroModel",
-    version="1.0.0",
+    version="1.0.1",
     author="Ernan Hughes",
     author_email="ernanhughes@gmail.com",
     description="Zero-Model Intelligence: Spatially-optimized visual policy maps",

d2ac257bff8ae94f329f6369068992bfd46e81cc
ernanhughes <155574808+ernanhughes@users.noreply.github.com>
2025-08-04 20:02:49 +0100

    change version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ZeroModel",
-    version="1.0.1",
+    version="1.0.2",
     author="Ernan Hughes",
     author_email="ernanhughes@gmail.com",
     description="Zero-Model Intelligence: Spatially-optimized visual policy maps",
